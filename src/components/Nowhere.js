cceb3bbe774db770751870887b4695fb4706d74e
VentsislavYankov <99548003+VentsislavYankov@users.noreply.github.com>
2023-01-18 15:14:34 +0200

    Add files via upload
    Without "node_modules" folder

---
+++ src/components/Nowhere.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nowhere = () => {
+  return (
+    <div>
+      <h1>Nowhere to hide</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ratione!
+        Pariatur necessitatibus fuga earum cupiditate, officiis repellendus
+        voluptate expedita laudantium quibusdam tempore, iusto laboriosam minima
+        velit possimus! Dolore, reprehenderit atque?
+      </p>
+    </div>
+  );
+};
+
+export default Nowhere;
---
+++ src/data/productData.js
@@ -0,0 +1,116 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    price: 999,
+    description:
+      "Apple iPhone 12 Pro has a bright and clear (resolution of 2532x1170) 6.1 inches-inch display and dimensions of  146,7 x 71,5 x 7,4mm. It also only weighs 189g. The main specifications are the Apple CPU - A14 Bionic, 128GB storage and 6Gb RAM.",
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/22/11/53/iphone-12-5766344_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: 2,
+    name: "iPhone 13 Pro Max",
+    price: 1099,
+    description:
+      "Apple iPhone 13 Pro Max has a bright and clear (resolution of 2778x1284) 6.7 inches-inch display and dimensions of  160.8 x 78.1 x 7.7mm. It also only weighs 240g. The main specifications are the Apple CPU - A15 Bionic, 128GB storage and 6Gb RAM.",
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/18/13/51/iphone-12-5755365_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    id: 3,
+    name: "Laptop",
+    price: 599,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/10/10/21/46/laptop-2838914_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    id: 4,
+    name: "Macbook Pro",
+    price: 1299,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2015/11/19/11/45/macbook-pro-1050973_1280.jpg",
+    brand: "Apple",
+    category: "Laptops",
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    id: 5,
+    name: "Macbook Air",
+    price: 999,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/07/21/29/macbook-pro-2381729_1280.jpg",
+    brand: "Apple",
+    category: "Laptops",
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    id: 6,
+    name: "iPhone 13 Pro",
+    price: 1299,
+    description:
+      "Apple iPhone 13 Pro weighs 203g with dimensions 146,7 x 71,5 x 7,6mm and a 6.1 inches-inch display. The screen resolution is 2532x1170 pixels, the image is bright and clear. The rear camera has a 12Mp sensor. For high-quality selfies, the front camera in 12Mp is responsible",
+    image:
+      "https://cdn.pixabay.com/photo/2022/09/25/22/25/iphones-7479304_960_720.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    id: 7,
+    name: "iPhone 13 Mini",
+    price: 899,
+    description:
+      "iphone 13 Mini is the was introduced to the market in 2021 and with a major upgrade from its predecessors. The Phone comes with Super Retina XDR OLED and an IP68 dust/water resistant. This makes the phone more durable and with that super retina display, the colors are more vibrant and sharp. Here are some of the features of Iphone 13 Mini ",
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/18/13/51/iphone-12-5755362_960_720.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    id: 8,
+    name: "iPhone 14",
+    price: 1599,
+    description:
+      "The iPhone 14 smartphone features a 6.1 inches Super Retina XDR OLED display with a resolution of 1170 x 2532 pixels at a pixel density of 460 pixels per inch (ppi) and 19.5:9 screen ratio. Hence, you can have an immersive viewing experience while watching videos and playing games with crystal clear images.",
+    image:
+      "https://cdn.pixabay.com/photo/2022/09/25/22/25/iphone-7479301_960_720.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+];
+
+export default products;
